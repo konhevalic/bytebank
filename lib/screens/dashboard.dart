402f1aea4288a5d56e4a1df4001c3235fc8b19e7
konhevalic <alan.patrick.konhevalic@gmail.com>
2021-09-29 22:12:47 -0300

    implementando lista de contatos

---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,59 @@
+import 'package:bytebank/screens/lista_contatos.dart';
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Dashboard'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('./images/bytebank.jpeg'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Material(
+                color: Colors.green[700],
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) { 
+                      return const ListaContatos();
+                    }));
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    height: 100,
+                    width: 150,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Icon(
+                          Icons.people,
+                          color: Colors.white),
+                        Text(
+                          'Contatos', 
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            ),
+                          )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+  }
+}
